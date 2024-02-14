8fcfa090fc98d72cabe7d3eabc0653b34f340a07
Gatão <gabrielgianotti5@gmail.com>
2024-02-14 14:52:16 +0000

    init

---
+++ src/test/java/com/gatao/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatao.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
